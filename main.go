c8c17c6ada6c895dd3ba8f44759ef4a82e228b17
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2019-11-28 01:15:18 +0900

    add append with index

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strconv"
 	"sync"
+	"time"
 )
 
 func main() {
@@ -11,32 +12,34 @@ func main() {
 	// slice := make([]int, 10)
 	// slice := make([]int, 0)
 	// slice := []int{}
-	slice := make([]string, 10)
+	slice := make([]string, 10000)
+	// capacityをとってlenを0にしておけば10個の0が入ることはない。
+	slice2 := make([]int, 0, 10000)
 	wg := sync.WaitGroup{}
-	// m := sync.Mutex{}
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
+	m := sync.Mutex{}
+	for i := 0; i < 10000; i++ {
+		wg.Add(2)
 		i := i
+		go appendSlice(&wg, slice, i)
 		go func() {
 			defer wg.Done()
-			slice[i] = strconv.Itoa(i)
-			// m.Lock()
-			// slice = append(slice, strconv.Itoa(i))
-			// m.Unlock()
+			m.Lock()
+			// 10000個の並行処理が、1つの共有のリソースに対して、配列を読み込む、変数を読み込む、新しい配列の作成、既存の配列への代入を行っている。appendはアトミックな操作ではないためデータ壊れる可能性がある。Lockして防ぐ
+			slice2 = append(slice2, i)
+			m.Unlock()
 		}()
 	}
 	wg.Wait()
 
-	fmt.Println(slice)
+	fmt.Println(len(slice))
+	fmt.Println(len(slice2))
 }
 
-/**
-appendを失敗させる
-配列を用意する
-sync.Waitを用意する
-forを10回回す
-group.Addする
-appendをする
-group.Doneをする
-group.Waitする。
-*/
+func appendSlice(wg *sync.WaitGroup, slice []string, i int) {
+	defer wg.Done()
+	time.Sleep(1 * time.Second)
+	slice[i] = strconv.Itoa(i)
+	// m.Lock()
+	// slice = append(slice, strconv.Itoa(i))
+	// m.Unlock()
+}
